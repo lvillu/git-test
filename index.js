34c148012b2ac39f691ac6194e5f3ca086c8ae95
Lauro Villarreal <lauro.villarreal@r2devpros.com>
2024-03-20 19:12:28 -0600

    Codigo formateado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 
-const app = express ();
+const app = express();
 app.use(express.json());
 
 
@@ -9,14 +9,13 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
-  });
-  
+});
 
-  app.get('/status', (request, response) => {
+
+app.get('/status', (request, response) => {
     const status = {
-       Status: 'Running'
+        Status: 'Running'
     };
-    
+
     response.send(status);
- });
- 
+});
